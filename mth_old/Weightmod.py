815f1c8944010161730080641bb3663006ce71fe
kawaho <kho2@nd.edu>
2020-06-10 16:41:51 -0500

    Pushing to own repo

--- mth_old/Weightmod.py
+++
@@ -1,7 +0,0 @@
-import glob
-lumifiles = []
-lumifiles.extend(glob.glob('inputs/MC2018_Dec/*.lumicalc.sum'))
-for x in lumifiles:
-  with open(x, 'w') as f:
-    f.write("59261.706")
-#print(json.dumps(weights, indent=4, sort_keys=True))
