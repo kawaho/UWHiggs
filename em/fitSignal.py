0ecff06c3251e3343de0d6a782e3c3c4e896043f
kawaho <kho2@nd.edu>
2020-08-17 23:30:08 -0500

    output results to ws

--- em/fitSignal.py
+++ em/fitSignal.py
@@ -1,6 +1,8 @@
 import ROOT
 import glob
 
+wsname = "CMS_emu_workspace"
+ws = ROOT.RooWorkspace(wsname, wsname)
 gcs = []
 mhyp = 125
 numGaussians = 2
@@ -60,19 +62,24 @@ def makeSumOfGaussians(pdfName, recoMassVar, mhypVar, deltaMuVars, sigmaVars, fr
     return ROOT.RooAddPdf(pdfName, pdfName, pdfs, coeffs, True)
 
 files = []
-files.extend(glob.glob('results_root/Data2017JEC/AnalyzeEM/VBF*.root')) 
+files.extend(glob.glob('results/Data2017JEC/AnalyzeEM/VBF*.root')) 
+recoMassVar = ROOT.RooRealVar("M_{e#mu}", "M_{e#mu}", 110, 140)
+getattr(ws, 'import')(recoMassVar)
 for file in files:
   f = ROOT.TFile(file)
   proc = "gg" if bool('GluGlu_LFV' in file) else "vbf"
+  suffix = "_".join([proc, "0",])
   hh = f.Get("TightOS/e_m_Mass")
-  recoMassVar = ROOT.RooRealVar("Meu", "Meu", 110, 140)
-  dh = ROOT.RooDataHist("dh", "dh", ROOT.RooArgList(recoMassVar), ROOT.RooFit.Import(hh))
+  dh = ROOT.RooDataHist("dh_" + suffix, "dh_" + suffix, ROOT.RooArgList(recoMassVar), ROOT.RooFit.Import(hh))
+#  getattr(ws, 'import')(dh)
+
   dmuvars = [ROOT.RooRealVar(makeGaussianVarname("dmu", proc, mhyp, 0, gaussIndex), "delta mu", gaussIndex, -1.5, +1.5) for gaussIndex in range(numGaussians)]
   sigmavars = [ ROOT.RooRealVar(makeGaussianVarname("sigma", proc, mhyp, 0, gaussIndex), "sigma", 1 + gaussIndex, 0.01, 10) for gaussIndex in range(numGaussians)]
   fractionvars = [ ROOT.RooRealVar(makeGaussianVarname("frac", proc, mhyp, 0, gaussIndex), "fraction variable for Gaussian sum", 0.5, 0, 1) for gaussIndex in range(numGaussians - 1)]
-  suffix = "_".join([proc, "0",])
+
   pdf = makeSumOfGaussians("sigpdf_" + suffix, recoMassVar, mhyp, dmuvars, sigmavars, fractionvars)
   pdf.fitTo(dh, ROOT.RooFit.Minimizer("Minuit2"), ROOT.RooFit.Range(mhyp - 5,mhyp + 5), ROOT.RooFit.SumW2Error(False))
+  getattr(ws, 'import')(pdf, ROOT.RooFit.RecycleConflictNodes())
 
 ##Do single Gaussian Fit
 #  mean = ROOT.RooRealVar("mean","Mean of Gaussian",mhyp,mhyp-1.5,mhyp+1.5)
@@ -86,6 +93,9 @@ for file in files:
 
   frame = recoMassVar.frame()
   dh.plotOn(frame)
-  gauss.plotOn(frame)
+  pdf.plotOn(frame)
   frame.Draw()
   canvas.SaveAs(makeGaussianVarname("plot", proc, mhyp, 0, gaussIndex) + ".png")
+
+ws.Print()
+ws.writeToFile("emu_ws.root")
