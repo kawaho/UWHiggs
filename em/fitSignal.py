c18182fb481744476effda2438c164252d0e47ac
kawaho <kho2@nd.edu>
2020-08-18 15:50:43 -0500

    Added Calculation of Eff Sigma

--- em/fitSignal.py
+++ em/fitSignal.py
@@ -1,11 +1,14 @@
 import ROOT
 import glob
 
+ROOT.gROOT.SetBatch(True)
 wsname = "CMS_emu_workspace"
 ws = ROOT.RooWorkspace(wsname, wsname)
 gcs = []
 mhyp = 125
 numGaussians = 2
+effsigma = 0.683
+f_effsigma = open("f_effsigma.txt", "w")
 canvas = ROOT.TCanvas('canvas','canvas',800,800)
 
 def makeGaussianVarname(varname, proc, mhyp, catname, gaussIndex):
@@ -61,41 +64,75 @@ def makeSumOfGaussians(pdfName, recoMassVar, mhypVar, deltaMuVars, sigmaVars, fr
 
     return ROOT.RooAddPdf(pdfName, pdfName, pdfs, coeffs, True)
 
+def findEffSigma(cdf, recoMassVar, count = 1, sigRange = 0, dir_ = None):
+  if dir_ == None:
+    lower_sig = cdf.findRoot(recoMassVar, 110, 140, (1 - effsigma)/2)
+    upper_sig = cdf.findRoot(recoMassVar, 110, 140, (1 + effsigma)/2)
+    sigRange  = upper_sig - lower_sig
+    if dir_ != "DOWN": 
+      lower_sigU = cdf.findRoot(recoMassVar, 110, 140, (1 - effsigma)/2 + 0.1*count)
+      upper_sigU = cdf.findRoot(recoMassVar, 110, 140, (1 + effsigma)/2 + 0.1*count)
+      sigRangeUp  = upper_sigU - lower_sigU
+    if dir_ != "UP":
+      lower_sigD = cdf.findRoot(recoMassVar, 110, 140, (1 - effsigma)/2 - 0.1*count)
+      upper_sigD = cdf.findRoot(recoMassVar, 110, 140, (1 + effsigma)/2 - 0.1*count)
+      sigRangeDown  = upper_sigD - lower_sigD
+   
+    if sigRange > sigRangeUp:
+      findEffSigma(cdf, recoMassVar, count+1 , sigRangeUp, "UP")
+    elif sigRange > sigRangeDown:
+      findEffSigma(cdf, recoMassVar, count+1, sigRangeDown, "DOWN")
+    elif sigRange < sigRangeUp and sigRange < sigRangeDown:
+      return sigRange
+    else:
+      return -9999
+     
 files = []
-files.extend(glob.glob('results/Data2017JEC/AnalyzeEM/VBF*.root')) 
+files.extend(glob.glob('results/Data2017JEC/AnalyzeEM/[!data]*.root')) 
 recoMassVar = ROOT.RooRealVar("M_{e#mu}", "M_{e#mu}", 110, 140)
 getattr(ws, 'import')(recoMassVar)
-for file in files:
-  f = ROOT.TFile(file)
-  proc = "gg" if bool('GluGlu_LFV' in file) else "vbf"
-  suffix = "_".join([proc, "0",])
-  hh = f.Get("TightOS/e_m_Mass")
-  dh = ROOT.RooDataHist("dh_" + suffix, "dh_" + suffix, ROOT.RooArgList(recoMassVar), ROOT.RooFit.Import(hh))
-#  getattr(ws, 'import')(dh)
+for f in files:
+  proc = "gg" if bool('GluGlu_LFV' in f) else "vbf"
+  file = ROOT.TFile(f)
+  file.cd()
+  for f in file.GetListOfKeys():
+    f = f.ReadObj()
+    f.cd()
+    cat = f.GetName().replace('TightOS', '')
+    for h in f.GetListOfKeys(): 
+      if h.GetName() == 'e_m_Mass':
+        hh = h.ReadObj()  
+        suffix = "_".join([proc, cat,]) 
+        dh = ROOT.RooDataHist("dh_" + suffix, "dh_" + suffix, ROOT.RooArgList(recoMassVar), ROOT.RooFit.Import(hh))
 
-  dmuvars = [ROOT.RooRealVar(makeGaussianVarname("dmu", proc, mhyp, 0, gaussIndex), "delta mu", gaussIndex, -1.5, +1.5) for gaussIndex in range(numGaussians)]
-  sigmavars = [ ROOT.RooRealVar(makeGaussianVarname("sigma", proc, mhyp, 0, gaussIndex), "sigma", 1 + gaussIndex, 0.01, 10) for gaussIndex in range(numGaussians)]
-  fractionvars = [ ROOT.RooRealVar(makeGaussianVarname("frac", proc, mhyp, 0, gaussIndex), "fraction variable for Gaussian sum", 0.5, 0, 1) for gaussIndex in range(numGaussians - 1)]
+        dmuvars = [ROOT.RooRealVar(makeGaussianVarname("dmu", proc, mhyp, cat, gaussIndex), "delta mu", gaussIndex, -1.5, +1.5) for gaussIndex in range(numGaussians)]
+        sigmavars = [ ROOT.RooRealVar(makeGaussianVarname("sigma", proc, mhyp, cat, gaussIndex), "sigma", 1 + gaussIndex, 0.01, 10) for gaussIndex in range(numGaussians)]
+        fractionvars = [ ROOT.RooRealVar(makeGaussianVarname("frac", proc, mhyp, cat, gaussIndex), "fraction variable for Gaussian sum", 0.5, 0, 1) for gaussIndex in range(numGaussians - 1)]
 
-  pdf = makeSumOfGaussians("sigpdf_" + suffix, recoMassVar, mhyp, dmuvars, sigmavars, fractionvars)
-  pdf.fitTo(dh, ROOT.RooFit.Minimizer("Minuit2"), ROOT.RooFit.Range(mhyp - 5,mhyp + 5), ROOT.RooFit.SumW2Error(False))
-  getattr(ws, 'import')(pdf, ROOT.RooFit.RecycleConflictNodes())
+        pdf = makeSumOfGaussians("sigpdf_" + suffix, recoMassVar, mhyp, dmuvars, sigmavars, fractionvars)
+        pdf.fitTo(dh, ROOT.RooFit.Minimizer("Minuit2"), ROOT.RooFit.Range(mhyp - 5,mhyp + 5), ROOT.RooFit.SumW2Error(False))
+        getattr(ws, 'import')(pdf, ROOT.RooFit.RecycleConflictNodes())
+        cdf = pdf.createCdf(ROOT.RooArgSet(recoMassVar))
+        result_effSigma = proc + "_" + cat + "\n" + str(findEffSigma(cdf, recoMassVar)) + "\n"
+        f_effsigma.write(result_effSigma)
 
-##Do single Gaussian Fit
-#  mean = ROOT.RooRealVar("mean","Mean of Gaussian",mhyp,mhyp-1.5,mhyp+1.5)
-#  sigma = ROOT.RooRealVar("sigma","Width of Gaussian",1,0.01,10) 
-#  pdf = ROOT.RooGaussian("gauss","gauss(x,mean,sigma)",recoMassVar,mean,sigma)
-#  pdf.fitTo(dh, ROOT.RooFit.Minimizer("Minuit2"), ROOT.RooFit.Range(mhyp - 5, mhyp + 5))
-
-#  sumWeights = dh.sumEntries()
-#  normVar = ROOT.RooRealVar(gauss.GetName() + "_norm", gauss.GetName() + "_norm", sumWeights, 0, sumWeights);
-#  normVar.setConstant(True)
-
-  frame = recoMassVar.frame()
-  dh.plotOn(frame)
-  pdf.plotOn(frame)
-  frame.Draw()
-  canvas.SaveAs(makeGaussianVarname("plot", proc, mhyp, 0, gaussIndex) + ".png")
-
+      ##Do single Gaussian Fit
+      #  mean = ROOT.RooRealVar("mean","Mean of Gaussian",mhyp,mhyp-1.5,mhyp+1.5)
+      #  sigma = ROOT.RooRealVar("sigma","Width of Gaussian",1,0.01,10) 
+      #  pdf = ROOT.RooGaussian("gauss","gauss(x,mean,sigma)",recoMassVar,mean,sigma)
+      #  pdf.fitTo(dh, ROOT.RooFit.Minimizer("Minuit2"), ROOT.RooFit.Range(mhyp - 5, mhyp + 5))
+      
+      #  sumWeights = dh.sumEntries()
+      #  normVar = ROOT.RooRealVar(gauss.GetName() + "_norm", gauss.GetName() + "_norm", sumWeights, 0, sumWeights);
+      #  normVar.setConstant(True)
+      
+        frame = recoMassVar.frame()
+        dh.plotOn(frame)
+        pdf.plotOn(frame)
+        frame.Draw()
+        canvas.SaveAs(makeGaussianVarname("plot", proc, mhyp, cat, gaussIndex) + ".png")
+    f_effsigma.write("\n")
+  f_effsigma.write("\n")
+f_effsigma.close()
 ws.Print()
 ws.writeToFile("emu_ws.root")
